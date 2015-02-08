fe51d249a50c81d110814371716e31a54d7bbead
Maxim Makankov <admin@Admins-Mac.local>
2015-02-08 10:47:00 +0300

    Initial Commit

---
+++ RMProject/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  RMProject
+//
+//  Created by mmakankov on 08/02/15.
+//  Copyright (c) 2015 mmakankov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
